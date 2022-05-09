7f01a3a2679e0386c3034257418fb201d3cd40f7
panos1962 <panos1962@gmail.com>
2022-05-09 09:02:38 +0300

    modifications

--- www/index.php
+++ www/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once("lib/selida.php");
 
+print ">>" . getcwd() . "<<";
+
 Selida::
 head()::
 body();
--- www/lib/selida.php
+++ www/lib/selida.php
@@ -4,7 +4,8 @@ require_once(preg_replace("/\/lib\/selida\.php$/", "", __FILE__) . "/../common/l
 
 class Selida {
 	private static $init_ok = FALSE;
-	public static $www;
+	public static $base_url;
+	public static $www_dir;
 
 	public static function init() {
 		if (self::$init_ok)
@@ -15,12 +16,14 @@ class Selida {
 		$server_name = $_SERVER["SERVER_NAME"];
 		switch ($server_name) {
 		case "tavladoros.gr":
-			self::$www = "http://" . $_SERVER["SERVER_NAME"];
+			self::$base_url = "http://" . $server_name;
 			break;
 		default:
-			self::$www = "http://" . $_SERVER["SERVER_NAME"] . "/tavli";
+			self::$base_url = "http://" . $_server_name . "/tavli";
 			break;
 		}
+
+		self::$www_dir = self::$base_dir . "/www";
 
 		return __CLASS__;
 	}
@@ -29,16 +32,15 @@ class Selida {
 ?>
 <html>
 <head>
-<link rel="icon" type="image/svg+xml" href="<?php print self::www("ikona/tavli.svg"); ?>">
-<title>
-<?php print $titlos; ?>
-</title>
+<link rel="icon" type="image/svg+xml" href="<?php print self::url("ikona/tavli.svg"); ?>">
+<title><?php print $titlos; ?></title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="<?php print Selida::www("lib/selida.css"); ?>">
 <?php
+		self::css("lib/selida");
 		self::javascript("lib/selida");
+
 		return __CLASS__;
 	}
 
@@ -59,40 +61,41 @@ class Selida {
 	}
 
 	public function stylesheet($css) {
+		$file = self::$www_dir . "/" . $script . ".js";
 ?>
-<link rel="stylesheet" href="<?php print $css; ?>">
+<link rel="stylesheet" href="<?php print $css; ?>.css">
 <?php
 	}
 
 	public function javascript($script) {
-		$file = Globals::$base_dir . "/www/" . $script . ".js";
+		if (substr($script, 0, 1) !== "/")
+		$file = self::$www_dir . "/" . $script . ".js";
 
 		if (!file_exists($file))
 		return;
 
 		$mtime = filemtime($file);
-		$file1 = Globals::$base_dir . "/www/" . $script . ".min.js";
 
-		if (file_exists($file1)) {
-			$mtime1 = filemtime($file1);
-			if ($mtime1 > $mtime) {
+		$filemin = self::$www_dir . "/" . $script . ".min.js";
+
+		if (file_exists($filemin)) {
+			$mtimemin = filemtime($filemin);
+			if ($mtimemin > $mtime) {
 				$script .= ".min";
-				$mtime = $mtime1;
+				$mtime = $mtimemin;
 			}
 		}
 ?>
-<script src="<?php
-print self::www($script); ?>.js?mt=<?php
-print $mtime; ?>"></script>
+<script src="<?php print $script; ?>.js?mt=<?php print $mtime; ?>"></script>
 <?php
 		return __CLASS__;
 	}
 
-	public function www($file) {
-		if (substr($file, 0, 1) === "/")
-		return self::$www . $file;
+	public function url($file) {
+		if (substr($file, 0, 1) !== "/")
+		$file = "/" . $file;
 
-		return self::$www . "/" . $file;
+		return self::$base_url . $file;
 	}
 }
 
