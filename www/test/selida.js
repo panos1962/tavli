af072197228dc8f0364edcb92e575c222977d9d2
panos1962 <panos1962@gmail.com>
2022-05-19 00:18:29 +0300

    modifications

--- www/test/selida.js
+++ www/test/selida.js
@@ -1,10 +1,26 @@
 "use strict";
 
 Selida.init = function() {
-	Selida.ofelimoDOM.
-	append($('<div>').
+	let tavliDOM = $('<div>').
 	attr('id', 'tavli').
 	append($('<img>').
 	attr('id', 'tavliBoard').
-	prop('src', '../ikona/tavli.svg')));
+	prop('src', '../ikona/tavli.svg')).
+	appendTo(Selida.ofelimoDOM);
+
+	tavliDOM.
+	append($('<div>').
+	addClass('zari').
+	addClass('zari0').
+	append($('<img>').
+	addClass('zariImage').
+	attr('src', '../ikona/zari/5.png')));
+
+	tavliDOM.
+	append($('<div>').
+	addClass('zari').
+	addClass('zari1').
+	append($('<img>').
+	addClass('zariImage').
+	attr('src', '../ikona/zari/6.png')));
 };
